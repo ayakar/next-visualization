1ebea8ff635583885cf720a526f99664a6cbf0c4
Aya Y <ayay@ayas-mbp.home>
2023-04-29 13:52:32 -0400

    add aggregatedRisk on line response

--- app/api/risks/line/route.ts
+++ app/api/risks/line/route.ts
@@ -9,9 +9,22 @@ export async function GET(request) {
     let transformedData: LineChartData = {};
     filtered.forEach((item: Risk) => {
         const year = item['Year'];
+        // Making aggregatedRisk
         const riskRating = item['Risk Rating'];
-        const current = transformedData[year] ? transformedData[year] : 0;
-        transformedData[year] = current + riskRating;
+        const current = transformedData[year] ? transformedData[year]['aggregatedRisk'] : 0;
+        const total = current + riskRating;
+
+        // Making riskFactors obj
+        // const riskFactors = item['Risk Factors'];
+        // Object.entries(riskFactors).forEach(([key, val]) => {
+        //     transformedData[year]['riskFactors'][key] = val
+        //     const current = transformedData[year] && transformedData[year]['Risk Factors'][key] ? transformedData[year]['Risk Factors'][key] : 0;
+        //     const total = current + val;
+        //     console.log(key, total);
+        // });
+
+        // Insert to transformedData
+        transformedData[year] = { aggregatedRisk: total };
     });
 
     return NextResponse.json(transformedData);
