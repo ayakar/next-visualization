364a1e6032d1194c25850fb147695594f73e89a9
Aya Y <ayay@ayas-mbp.home>
2023-04-30 18:06:46 -0400

    send only searched risk factor total to line chart

--- app/api/risks/line/route.ts
+++ app/api/risks/line/route.ts
@@ -5,42 +5,48 @@ import { LineChartData, Risk } from '@/app/types/RiskRating';
 
 interface TransformedData {
     [key: number]: {
-        aggregatedRisk: number;
         riskFactors: { [key: string]: number };
     };
 }
 
 export async function GET(request) {
-    console.log('line api called');
+    const { searchParams } = new URL(request.url);
+    const riskFactorParams = searchParams.get('risk-factor'); //  'Flooding,Volcano'
+    const riskFactorParamsArr = riskFactorParams
+        ? riskFactorParams.split(',')
+        : ['Earthquake', 'Extreme heat', 'Wildfire', 'Tornado', 'Flooding', 'Volcano', 'Hurricane', 'Drought', 'Extreme cold', 'Sea level rise'];
+
     const filtered = await filterRiskData(request);
+
     let transformedData: TransformedData = {};
     filtered.forEach((item: Risk) => {
         const year = item['Year'];
-        const riskRating = item['Risk Rating'];
         const riskFactors = item['Risk Factors'];
-        // Making aggregatedRisk
-        if (year in transformedData) {
-            transformedData[year]['aggregatedRisk'] += riskRating;
-        } else {
-            transformedData[year] = { aggregatedRisk: riskRating };
+        // Initializing transformedData {"YEAR":{}}
+        if (!(year in transformedData)) {
+            transformedData[year] = {};
         }
 
-        // Making riskFactors obj
+        // Making riskFactors obj ex. "riskFactors": { "Hurricane": 44.91000000000002,"Extreme heat": 52.25000000000004,}
         if (!('riskFactors' in transformedData[year])) {
             transformedData[year]['riskFactors'] = {};
         }
+        // Calculating each risk factor if it's in search param.
         Object.entries(riskFactors).forEach(([key, val]) => {
-            if (key in transformedData[year]['riskFactors']) {
+            if (key in transformedData[year]['riskFactors'] && riskFactorParamsArr.includes(key)) {
+                // Calculate value when the risk factor obj exists
                 transformedData[year]['riskFactors'][key] += val;
-            } else {
+            } else if (riskFactorParamsArr.includes(key)) {
+                // Initialize obj when the key doesn't exit in the year obj
                 transformedData[year]['riskFactors'] = { ...transformedData[year]['riskFactors'], [key]: val };
             }
         });
     });
     // Transforming to send
     let finalTransformedData: LineChartData[] | [] = [];
-    Object.entries(transformedData).forEach(([key, value]) => {
-        const obj = { year: key, aggregatedRisk: value.aggregatedRisk, riskFactors: value.riskFactors };
+    Object.entries(transformedData).forEach(([year, data]) => {
+        const aggregatedRisk: number = Object.values<number>(data.riskFactors).reduce((prev, curr) => prev + curr, 0);
+        const obj = { year: year, aggregatedRisk: aggregatedRisk, riskFactors: data.riskFactors };
         finalTransformedData.push(obj);
     });
 
