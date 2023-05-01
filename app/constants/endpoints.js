b2d7f6f5a6b9a6292ac1b2ff6052de48c6f731e1
Aya Y <ayay@ayas-mbp.home>
2023-05-01 15:51:28 -0400

    change constants file

--- app/constants/endpoints.js
+++ app/constants/endpoints.js
@@ -1,14 +1,11 @@
-const domain = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://risk-viz-git-development-ayakar.vercel.app/'; // TODO: fix this
-// const domain = process.env.DOMAIN;
-
 export const config = {
     url: {
-        RISKS_TABLE: `${domain}/api/risks/table`,
-        RISKS_LINE: `${domain}/api/risks/line`,
-        RISKS_MAP: `${domain}/api/risks/map`,
-        RISKS_YEARS: `${domain}/api/risks/years`,
-        RISKS_ASSETS: `${domain}/api/risks/assets`,
-        RISKS_LOCATIONS: `${domain}/api/risks/locations`,
-        RISKS_CATEGORIES: `${domain}/api/risks/business_categories`,
+        RISKS_TABLE: `/api/risks/table`,
+        RISKS_LINE: `/api/risks/line`,
+        RISKS_MAP: `/api/risks/map`,
+        RISKS_YEARS: `/api/risks/years`,
+        RISKS_ASSETS: `/api/risks/assets`,
+        RISKS_LOCATIONS: `/api/risks/locations`,
+        RISKS_CATEGORIES: `/api/risks/business_categories`,
     },
 };
