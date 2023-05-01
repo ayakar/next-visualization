0f762b102902db507a13adeb7eb8e48b775e3276
Aya Y <ayay@ayas-mbp.home>
2023-05-01 14:53:30 -0400

    remove non used files

--- app/api/risks/business_categories/route.ts
+++
@@ -1,8 +0,0 @@
-// TODO: not need
-// import { NextResponse } from 'next/server';
-// import { getBusinessCategories } from './getBusinessCategories';
-
-// export async function GET() {
-//     const businessCategoriesArrNum = getBusinessCategories();
-//     return NextResponse.json(businessCategoriesArrNum);
-// }
--- app/api/risks/years/route.ts
+++
@@ -1,9 +0,0 @@
-// TODO: not need
-// import { NextResponse } from 'next/server';
-// import { getYear } from './getYear';
-
-// export async function GET() {
-//     const yearArrNum = getYear();
-
-//     return NextResponse.json(yearArrNum);
-// }
